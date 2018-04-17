c3f9cbcf39a12ac18d6092705366a040b28853ee
rr- <rr-@sakuya.pl>
2018-04-17 10:03:55 +0200

    parser: support metadata

--- docstring_parser/parser.py
+++ docstring_parser/parser.py
@@ -1,5 +1,31 @@
 import inspect
+import re
 import typing as T
+
+
+class DocstringMeta:
+    """
+    Docstring meta information.
+
+    Symbolizes lines in form of
+
+        :param arg: description
+        :raises ValueError: if something happens
+    """
+
+    def __init__(
+            self,
+            args: T.List[str],
+            description: str
+    ) -> None:
+        """
+        Initialize self.
+
+        :param args: list of arguments before delimiting colon.
+        :param description: associated docstring description.
+        """
+        self.args = args
+        self.description = description
 
 
 class Docstring:
@@ -9,22 +35,52 @@ class Docstring:
         """Intializes self."""
         self.short_description: T.Optional[str] = None
         self.long_description: T.Optional[str] = None
-        self.nl_after_short_description = False
+        self.blank_after_short_description = False
+        self.blank_after_long_description = False
+        self.meta: T.List[DocstringMeta] = []
 
 
 def parse(text: str) -> Docstring:
     """
     Parse the docstring into its components.
+
     :returns: parsed docstring
     """
-    docstring = Docstring()
+    ret = Docstring()
+    if not text:
+        return ret
 
-    if text:
-        parts = inspect.cleandoc(text).split('\n', 1)
-        docstring.short_description = parts[0]
+    parts = inspect.cleandoc(text).split('\n', 1)
+    ret.short_description = parts[0]
+    if len(parts) <= 1:
+        return ret
 
-        if len(parts) > 1:
-            docstring.nl_after_short_description = parts[1].startswith('\n')
-            docstring.long_description = parts[1].strip()
+    rest = parts[1]
+    match = re.search('^:', rest, flags=re.M)
+    if match:
+        long_desc_chunk = rest[:match.start()]
+        meta_chunk = rest[match.start():]
+    else:
+        long_desc_chunk = rest
+        meta_chunk = ''
+    ret.blank_after_short_description = long_desc_chunk.startswith('\n')
+    ret.blank_after_long_description = long_desc_chunk.endswith('\n\n')
+    ret.long_description = long_desc_chunk.strip()
+    if not ret.long_description:
+        ret.long_description = None
 
-    return docstring
+    for match in re.finditer(
+            r'(^:.*?)(?=^:|\Z)', meta_chunk, flags=re.S | re.M
+    ):
+        chunk = match.group(0)
+        if not chunk:
+            continue
+        args_chunk, desc_chunk = chunk.lstrip(':').split(':', 1)
+        args = args_chunk.split()
+        desc = desc_chunk.strip()
+        if '\n' in desc:
+            first_line, rest = desc.split('\n', 1)
+            desc = first_line + '\n' + inspect.cleandoc(rest)
+        ret.meta.append(DocstringMeta(args, description=desc))
+
+    return ret
--- docstring_parser/tests/test_parser.py
+++ docstring_parser/tests/test_parser.py
@@ -1,3 +1,5 @@
+import typing as T
+
 import pytest
 
 from docstring_parser import parse
@@ -12,10 +14,11 @@ def test_short_description(source: str, expected: str) -> None:
     docstring = parse(source)
     assert docstring.short_description == expected
     assert docstring.long_description is None
+    assert docstring.meta == []
 
 
 @pytest.mark.parametrize(
-    'source, expected_short_desc, expected_long_desc, expected_nl',
+    'source, expected_short_desc, expected_long_desc, expected_blank',
     [
         (
             'Short description\n\nLong description',
@@ -87,9 +90,117 @@ def test_long_description(
         source: str,
         expected_short_desc: str,
         expected_long_desc: str,
-        expected_nl: bool
+        expected_blank: bool
 ) -> None:
     docstring = parse(source)
     assert docstring.short_description == expected_short_desc
     assert docstring.long_description == expected_long_desc
-    assert docstring.nl_after_short_description == expected_nl
+    assert docstring.blank_after_short_description == expected_blank
+    assert docstring.meta == []
+
+
+@pytest.mark.parametrize(
+    'source, expected_long_desc, '
+    'expected_blank_short_desc, expected_blank_long_desc',
+    [
+        (
+            '''
+            Short description
+            :param: asd
+            ''',
+            None, False, False
+        ),
+
+        (
+            '''
+            Short description
+            Long description
+            :param: asd
+            ''',
+            'Long description', False, False
+        ),
+
+        (
+            '''
+            Short description
+            Long description
+                Second line
+            :param: asd
+            ''',
+            'Long description\n    Second line', False, False
+        ),
+
+        (
+            '''
+            Short description
+
+            Long description
+                Second line
+            :param: asd
+            ''',
+            'Long description\n    Second line', True, False
+        ),
+
+        (
+            '''
+            Short description
+
+            Long description
+                Second line
+
+            :param: asd
+            ''',
+            'Long description\n    Second line', True, True
+        )
+    ]
+)
+def test_meta_newlines(
+        source: str,
+        expected_long_desc: T.Optional[str],
+        expected_blank_short_desc: bool,
+        expected_blank_long_desc: bool
+) -> None:
+    docstring = parse(source)
+    assert docstring.short_description == 'Short description'
+    assert docstring.long_description == expected_long_desc
+    assert docstring.blank_after_short_description == expected_blank_short_desc
+    assert docstring.blank_after_long_description == expected_blank_long_desc
+    assert len(docstring.meta) == 1
+
+
+def test_meta_with_multiline_description() -> None:
+    docstring = parse(
+        '''
+        Short description
+
+        :param: asd
+            1
+                2
+            3
+        ''')
+    assert docstring.short_description == 'Short description'
+    assert len(docstring.meta) == 1
+    assert docstring.meta[0].args == ['param']
+    assert docstring.meta[0].description == 'asd\n1\n    2\n3'
+
+
+def test_multiple_meta() -> None:
+    docstring = parse(
+        '''
+        Short description
+
+        :param: asd
+            1
+                2
+            3
+        :param2: herp
+        :param3: derp
+        ''')
+    assert docstring.short_description == 'Short description'
+    assert len(docstring.meta) == 3
+    assert docstring.meta[0].args == ['param']
+    assert docstring.meta[0].description == 'asd\n1\n    2\n3'
+    assert docstring.meta[1].args == ['param2']
+    assert docstring.meta[1].description == 'herp'
+    assert docstring.meta[2].args == ['param3']
+    assert docstring.meta[2].description == 'derp'
