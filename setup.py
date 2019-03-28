2a681c257e8066fa8c123914be727302bdb02a0d
Laurie Opperman <laurie_opperman@hotmail.com>
2019-03-28 18:40:32 +1000

    Fix content-type for long-description

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author_email='rr-@sakuya.pl',
     name='docstring_parser',
     long_description=(Path(__file__).parent / 'README.md').read_text(),
+    long_description_content_type='text/markdown',
     version='0.1',
     url='https://github.com/rr-/docstring_parser',
     packages=find_packages(),
