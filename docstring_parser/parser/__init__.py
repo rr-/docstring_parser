923d96f664791cad04b1573ed742ef85178b4236
Laurie Opperman <laurie_opperman@hotmail.com>
2019-03-29 10:22:47 +1000

    Clean-up, update doc

--- docstring_parser/parser/__init__.py
+++ docstring_parser/parser/__init__.py
@@ -7,35 +7,23 @@ from .common import ParseError, Docstring
 _styles = {"rest": rest.parse, "google": google.parse}
 
 
-def _parse_score(docstring: Docstring) -> int:
-    """
-    Produce a score for the parsing.
-
-    :param Docstring docstring: parsed docstring representation
-    :returns int: parse score, higher is better
-    """
-
-    return len(docstring.meta)
-
-
 def parse(text: str, style: str = 'auto') -> Docstring:
     """
     Parse the docstring into its components.
 
-    :param str text: docstring text to parse
-    :param str style: docstring style, choose from: 'rest', 'auto'
-    :returns Docstring: parsed docstring representation
+    :param text: docstring text to parse
+    :param style: docstring style, choose from: 'rest', 'google', 'auto'
+    :returns: parsed docstring representation
     """
 
-    if style == 'auto':
-        rets = []
-        for _parse in _styles.values():
-            try:
-                rets.append(_parse(text))
-            except ParseError as e:
-                exc = e
-        if not rets:
-            raise exc
-        return sorted(rets, key=_parse_score, reverse=True)[0]
-    else:
+    if style != 'auto':
         return _styles[style](text)
+    rets = []
+    for parse_ in _styles.values():
+        try:
+            rets.append(parse_(text))
+        except ParseError as e:
+            exc = e
+    if not rets:
+        raise exc
+    return sorted(rets, key=lambda d: len(d.meta), reverse=True)[0]
