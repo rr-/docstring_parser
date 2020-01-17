3b1439b4bc2f4187af01d3287586a8d20def716f
rr- <rr-@sakuya.pl>
2020-01-17 20:10:53 +0100

    build: shift to setup.cfg

--- setup.py
+++ setup.py
@@ -1,28 +1,3 @@
-from pathlib import Path
+import setuptools
 
-from setuptools import find_packages, setup
-
-setup(
-    author="Marcin Kurczewski",
-    author_email="rr-@sakuya.pl",
-    name="docstring_parser",
-    long_description=(Path(__file__).parent / "README.md").read_text(),
-    long_description_content_type="text/markdown",
-    version="0.5",
-    url="https://github.com/rr-/docstring_parser",
-    packages=find_packages(),
-    package_dir={"docstring_parser": "docstring_parser"},
-    package_data={"docstring_parser": ["../LICENSE.md"]},
-    classifiers=[
-        "Environment :: Other Environment",
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
-        "Topic :: Documentation :: Sphinx",
-        "Topic :: Text Processing :: Markup",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-)
+setuptools.setup()
