6b3b74a0249228e325b2f513d0c1142421614f20
munagekar <avm.abhishek@gmail.com>
2020-09-22 23:37:28 +0900

    add tests

--- docstring_parser/tests/test_google.py
+++ docstring_parser/tests/test_google.py
@@ -591,3 +591,28 @@ def test_broken_meta() -> None:
 
     with pytest.raises(ParseError):
         parse("Args:\n    herp derp")
+
+
+def test_unknown_headers() -> None:
+    docstring = parse(
+        """Short desc
+        
+        Unknown 0:
+            title0: content0
+
+        Args:
+            arg0: desc0
+            arg1: desc1
+
+        Unknown1:
+            title1: content1
+
+        Unknown2:
+            title2: content2
+        """
+    )
+
+    assert docstring.params[0].arg_name == "arg0"
+    assert docstring.params[0].description == "desc0"
+    assert docstring.params[1].arg_name == "arg1"
+    assert docstring.params[1].description == "desc1"
