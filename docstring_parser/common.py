56258de7f5e6ba39c1985b2202889dbd0a4dfb4a
Marcin Kurczewski <dash@wind.garden>
2022-04-25 14:36:56 +0200

    numpydoc: improve support for examples (#65)
    Resolves #64

--- docstring_parser/common.py
+++ docstring_parser/common.py
@@ -137,10 +137,12 @@ class DocstringExample(DocstringMeta):
     def __init__(
         self,
         args: T.List[str],
+        snippet: T.Optional[str],
         description: T.Optional[str],
     ) -> None:
         """Initialize self."""
         super().__init__(args, description)
+        self.snippet = snippet
         self.description = description
 
 
--- docstring_parser/numpydoc.py
+++ docstring_parser/numpydoc.py
@@ -7,10 +7,12 @@ import inspect
 import itertools
 import re
 import typing as T
+from textwrap import dedent
 
 from .common import (
     Docstring,
     DocstringDeprecated,
+    DocstringExample,
     DocstringMeta,
     DocstringParam,
     DocstringRaises,
@@ -223,6 +225,44 @@ class DeprecationSection(_SphinxSection):
         )
 
 
+class ExamplesSection(Section):
+    """Parser for numpydoc examples sections.
+
+    E.g. any section that looks like this:
+        >>> import numpy.matlib
+        >>> np.matlib.empty((2, 2))    # filled with random data
+        matrix([[  6.76425276e-320,   9.79033856e-307], # random
+                [  7.39337286e-309,   3.22135945e-309]])
+        >>> np.matlib.empty((2, 2), dtype=int)
+        matrix([[ 6600475,        0], # random
+                [ 6586976, 22740995]])
+    """
+
+    def parse(self, text: str) -> T.Iterable[DocstringMeta]:
+        """Parse ``DocstringExample`` objects from the body of this section.
+
+        :param text: section body text. Should be cleaned with
+                     ``inspect.cleandoc`` before parsing.
+        """
+        lines = dedent(text).strip().splitlines()
+        while lines:
+            snippet_lines = []
+            description_lines = []
+            while lines:
+                if not lines[0].startswith(">>>"):
+                    break
+                snippet_lines.append(lines.pop(0))
+            while lines:
+                if lines[0].startswith(">>>"):
+                    break
+                description_lines.append(lines.pop(0))
+            yield DocstringExample(
+                [self.key],
+                snippet="\n".join(snippet_lines) if snippet_lines else None,
+                description="\n".join(description_lines),
+            )
+
+
 DEFAULT_SECTIONS = [
     ParamSection("Parameters", "param"),
     ParamSection("Params", "param"),
@@ -244,8 +284,8 @@ DEFAULT_SECTIONS = [
     ReturnsSection("Return", "returns"),
     YieldsSection("Yields", "yields"),
     YieldsSection("Yield", "yields"),
-    Section("Examples", "examples"),
-    Section("Example", "examples"),
+    ExamplesSection("Examples", "examples"),
+    ExamplesSection("Example", "examples"),
     Section("Warnings", "warnings"),
     Section("Warning", "warnings"),
     Section("See Also", "see_also"),
