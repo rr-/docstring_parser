rr-/docstring_parser

commit ba7c874cd7bb483f9af4c9fdd2e0f3c68f446e0a

style: bump pre-commit packages; reformat

.pre-commit-config.yaml
@@ -1,16 +1,16 @@
 repos:
 
 - repo: https://github.com/pycqa/isort
-  rev: 5.9.2
+  rev: 5.10.1
   hooks:
   - id: isort
     additional_dependencies: [toml]
 - repo: https://github.com/psf/black
-  rev: 21.6b0
+  rev: 22.3.0
   hooks:
   - id: black
 - repo: https://github.com/pycqa/pylint
-  rev: v2.9.5
+  rev: v2.13.5
   hooks:
   - id: pylint
     additional_dependencies: [toml]

docstring_parser/parser.py
@@ -1,5 +1,7 @@
 """The main parsing routine."""
 
+import typing as T
+
 from docstring_parser import epydoc, google, numpydoc, rest
 from docstring_parser.common import (
     Docstring,
@@ -26,14 +28,17 @@ def parse(text: str, style: DocstringStyle = DocstringStyle.AUTO) -> Docstring:
     if style != DocstringStyle.AUTO:
         return _STYLE_MAP[style].parse(text)
 
+    exc: T.Optional[Exception] = None
     rets = []
     for module in _STYLE_MAP.values():
         try:
-            rets.append(module.parse(text))
+            ret = module.parse(text)
         except ParseError as ex:
             exc = ex
+        else:
+            rets.append(ret)
 
-    if not rets:
+    if exc is not None:
         raise exc
 
     return sorted(rets, key=lambda d: len(d.meta), reverse=True)[0]

docstring_parser/epydoc.py
@@ -76,9 +76,7 @@ def parse(text: str) -> Docstring:
 
         match = param_match or raise_match or return_match or meta_match
         if not match:
-            raise ParseError(
-                'Error parsing meta information near "{}".'.format(chunk)
-            )
+            raise ParseError(f'Error parsing meta information near "{chunk}".')
 
         desc_chunk = chunk[match.end() :]
         if param_match:
@@ -111,7 +109,7 @@ def parse(text: str) -> Docstring:
                 "ytype",
             ]:
                 raise ParseError(
-                    'Error parsing meta information near "{}".'.format(chunk)
+                    f'Error parsing meta information near "{chunk}".'
                 )
 
         desc = desc_chunk.strip()
@@ -135,7 +133,7 @@ def parse(text: str) -> Docstring:
             is_generator = key in {"ytype", "yield"}
             if info.setdefault("is_generator", is_generator) != is_generator:
                 raise ParseError(
-                    'Error parsing meta information for "{}".'.format(arg_name)
+                    f'Error parsing meta information for "{arg_name}".'
                 )
 
     is_done: T.Dict[str, bool] = {}

docstring_parser/google.py
@@ -85,7 +85,7 @@ class GoogleParser:
             colon = ""
         self.titles_re = re.compile(
             "^("
-            + "|".join("(%s)" % t for t in self.sections)
+            + "|".join(f"({t})" for t in self.sections)
             + ")"
             + colon
             + "[ \t\r\f\v]*$",
@@ -109,7 +109,7 @@ class GoogleParser:
             return self._build_single_meta(section, text)
 
         if ":" not in text:
-            raise ParseError("Expected a colon in {}.".format(repr(text)))
+            raise ParseError(f"Expected a colon in {text!r}.")
 
         # Split spec and description
         before, desc = text.split(":", 1)
@@ -257,7 +257,7 @@ class GoogleParser:
             # Determine indent
             indent_match = re.search(r"^\s*", chunk)
             if not indent_match:
-                raise ParseError('Can\'t infer indent from "{}"'.format(chunk))
+                raise ParseError(f'Can\'t infer indent from "{chunk}"')
             indent = indent_match.group()
 
             # Check for singular elements
@@ -273,9 +273,7 @@ class GoogleParser:
             _re = "^" + indent + r"(?=\S)"
             c_matches = list(re.finditer(_re, chunk, flags=re.M))
             if not c_matches:
-                raise ParseError(
-                    'No specification for "{}": "{}"'.format(title, chunk)
-                )
+                raise ParseError(f'No specification for "{title}": "{chunk}"')
             c_splits = []
             for j in range(len(c_matches) - 1):
                 c_splits.append((c_matches[j].end(), c_matches[j + 1].start()))

docstring_parser/numpydoc.py
@@ -67,7 +67,8 @@ class Section:
         This pattern will match this instance's ``title`` attribute in
         an anonymous group.
         """
-        return r"^({})\s*?\n{}\s*$".format(self.title, "-" * len(self.title))
+        dashes = "-" * len(self.title)
+        return rf"^({self.title})\s*?\n{dashes}\s*$"
 
     def parse(self, text: str) -> T.Iterable[DocstringMeta]:
         """Parse ``DocstringMeta`` objects from the body of this section.
@@ -112,7 +113,7 @@ class _SphinxSection(Section):
 
     @property
     def title_pattern(self) -> str:
-        return r"^\.\.\s*({})\s*::".format(self.title)
+        return rf"^\.\.\s*({self.title})\s*::"
 
 
 class ParamSection(_KVSection):

docstring_parser/rest.py
@@ -39,7 +39,7 @@ def _build_meta(args: T.List[str], desc: str) -> DocstringMeta:
             is_optional = None
         else:
             raise ParseError(
-                "Expected one or two arguments for a {} keyword.".format(key)
+                f"Expected one or two arguments for a {key} keyword."
             )
 
         match = re.match(r".*defaults to (.+)", desc, flags=re.DOTALL)
@@ -61,7 +61,7 @@ def _build_meta(args: T.List[str], desc: str) -> DocstringMeta:
             type_name = None
         else:
             raise ParseError(
-                "Expected one or no arguments for a {} keyword.".format(key)
+                f"Expected one or no arguments for a {key} keyword."
             )
 
         return DocstringReturns(
@@ -90,7 +90,7 @@ def _build_meta(args: T.List[str], desc: str) -> DocstringMeta:
             type_name = None
         else:
             raise ParseError(
-                "Expected one or no arguments for a {} keyword.".format(key)
+                f"Expected one or no arguments for a {key} keyword."
             )
         return DocstringRaises(
             args=args, description=desc, type_name=type_name
@@ -137,7 +137,7 @@ def parse(text: str) -> Docstring:
             args_chunk, desc_chunk = chunk.lstrip(":").split(":", 1)
         except ValueError as ex:
             raise ParseError(
-                'Error parsing meta information near "{}".'.format(chunk)
+                f'Error parsing meta information near "{chunk}".'
             ) from ex
         args = args_chunk.split()
         desc = desc_chunk.strip()

docstring_parser/tests/test_epydoc.py
@@ -21,7 +21,7 @@ def test_short_description(source: str, expected: str) -> None:
     docstring = parse(source)
     assert docstring.short_description == expected
     assert docstring.long_description is None
-    assert docstring.meta == []
+    assert not docstring.meta
 
 
 @pytest.mark.parametrize(
@@ -98,7 +98,7 @@ def test_long_description(
     assert docstring.short_description == expected_short_desc
     assert docstring.long_description == expected_long_desc
     assert docstring.blank_after_short_description == expected_blank
-    assert docstring.meta == []
+    assert not docstring.meta
 
 
 @pytest.mark.parametrize(

docstring_parser/tests/test_google.py
@@ -132,7 +132,7 @@ def test_short_description(source: str, expected: str) -> None:
     docstring = parse(source)
     assert docstring.short_description == expected
     assert docstring.long_description is None
-    assert docstring.meta == []
+    assert not docstring.meta
 
 
 @pytest.mark.parametrize(
@@ -209,7 +209,7 @@ def test_long_description(
     assert docstring.short_description == expected_short_desc
     assert docstring.long_description == expected_long_desc
     assert docstring.blank_after_short_description == expected_blank
-    assert docstring.meta == []
+    assert not docstring.meta
 
 
 @pytest.mark.parametrize(

commit c1bf4bde782100bd012a048c170ef6332857bb95

parser: fix autodetection error handling
Regression from ba7c874cd7bb483f9af4c9fdd2e0f3c68f446e0a.
Resolves #66.

.pre-commit-config.yaml
@@ -10,7 +10,7 @@ repos:
   hooks:
   - id: black
 - repo: https://github.com/pycqa/pylint
-  rev: v2.13.5
+  rev: v2.13.7
   hooks:
   - id: pylint
     additional_dependencies: [toml]

docstring_parser/parser.py
@@ -38,7 +38,7 @@ def parse(text: str, style: DocstringStyle = DocstringStyle.AUTO) -> Docstring:
         else:
             rets.append(ret)
 
-    if exc is not None:
+    if not rets:
         raise exc
 
     return sorted(rets, key=lambda d: len(d.meta), reverse=True)[0]

docstring_parser/tests/test_parser.py
@@ -1,5 +1,6 @@
 """Tests for generic docstring routines."""
-from docstring_parser.common import DocstringStyle
+import pytest
+from docstring_parser.common import DocstringStyle, ParseError
 from docstring_parser.parser import parse
 
 
@@ -180,3 +181,24 @@ def test_numpydoc() -> None:
     assert docstring.many_returns is not None
     assert len(docstring.many_returns) == 1
     assert docstring.many_returns[0] == docstring.returns
+
+
+def test_autodetection_error_detection() -> None:
+    """Test autodection for the case where one of the parsers throws an error
+    and another one succeeds.
+    """
+    source = """
+    Does something useless
+
+    :param 3 + 3 a: a param
+    """
+
+    with pytest.raises(ParseError):
+        # assert that one of the parsers does raise
+        parse(source, DocstringStyle.REST)
+
+    # assert that autodetection still works
+    docstring = parse(source)
+
+    assert docstring
+    assert docstring.style == DocstringStyle.GOOGLE