969b64467647d23cd85767b09c7fdcbacbbff0cf
Mauricio Villegas <5780272+mauvilsa@users.noreply.github.com>
2023-09-15 09:37:39 +0200

    misc: support Python 3.12 (#82)

--- docstring_parser/attrdoc.py
+++ docstring_parser/attrdoc.py
@@ -5,19 +5,23 @@
 
 import ast
 import inspect
+import sys
 import textwrap
 import typing as T
 from types import ModuleType
 
 from .common import Docstring, DocstringParam
 
-ast_constant_attr = {
-    ast.Constant: "value",
-    # python <= 3.7:
-    ast.NameConstant: "value",
-    ast.Num: "n",
-    ast.Str: "s",
-}
+ast_constant_attr = {ast.Constant: "value"}
+
+if sys.version_info[:2] <= (3, 7):
+    ast_constant_attr.update(
+        {
+            ast.NameConstant: "value",
+            ast.Num: "n",
+            ast.Str: "s",
+        }
+    )
 
 
 def ast_get_constant_value(node: ast.AST) -> T.Any:
